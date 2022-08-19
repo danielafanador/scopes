9653a5eb48142779329aef52e4929189bd60a394
Daniel Afanador <danielthemisfits@gmail.com>
2022-08-18 22:29:00 -0300

    Un programa con scopes y más de una función

---
+++ scopes.py
@@ -0,0 +1,24 @@
+e = 'mathematical constant'
+a_sum = 1000
+length = 50
+
+def exponential (x):
+    e = 2.72
+    print (e)
+    print ('')
+    print (e**x)    
+    return e**x
+    
+result = exponential (5)
+
+print ('')
+print (result)
+
+def divide ():
+    #print(a_sum)
+    #print (length)
+    return a_sum / length
+    
+result_2 = divide ()
+print ('')
+print (result_2)
